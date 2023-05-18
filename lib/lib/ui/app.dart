7616f33e584baec3f2985f9c3573c1fa7e69d694
valeferreiraan <78361578+valeferreiraan@users.noreply.github.com>
2023-05-18 12:56:52 -0500

    Add files via upload

---
+++ lib/lib/ui/app.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:greenplastic_app/ui/pages/authentication/signup.dart';
+import 'package:greenplastic_app/ui/pages/home.dart';
+import 'package:greenplastic_app/constants.dart';
+import 'pages/authentication/login.dart';
+import 'pages/content/detail.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Simple Flutter Navigation',
+      // This theme was made for FlexColorScheme version 6.1.1. Make sure
+// you use same or higher version, but still same major version. If
+// you use a lower version, some properties may not be supported. In
+// that case you can also remove them after copying the theme to your app.
+      theme: FlexThemeData.light(
+        scheme: FlexScheme.green,
+        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+        blendLevel: 9,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 10,
+          blendOnColors: false,
+          defaultRadius: 25.0,
+          elevatedButtonRadius: 40.0,
+          inputDecoratorRadius: 40.0,
+          inputDecoratorFocusedBorderWidth: 2.0,
+          bottomNavigationBarSelectedLabelSchemeColor:
+              SchemeColor.primaryContainer,
+          bottomNavigationBarUnselectedLabelSchemeColor:
+              SchemeColor.onPrimaryContainer,
+          bottomNavigationBarSelectedIconSchemeColor:
+              SchemeColor.primaryContainer,
+          bottomNavigationBarUnselectedIconSchemeColor:
+              SchemeColor.onPrimaryContainer,
+          navigationBarHeight: 55.0,
+        ),
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        // To use the playground font, add GoogleFonts package and uncomment
+        // fontFamily: GoogleFonts.notoSans().fontFamily,
+      ),
+      darkTheme: FlexThemeData.dark(
+        scheme: FlexScheme.green,
+        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+        blendLevel: 15,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 20,
+          defaultRadius: 25.0,
+          elevatedButtonRadius: 40.0,
+          inputDecoratorRadius: 40.0,
+          inputDecoratorFocusedBorderWidth: 2.0,
+          bottomNavigationBarSelectedLabelSchemeColor:
+              SchemeColor.primaryContainer,
+          bottomNavigationBarUnselectedLabelSchemeColor:
+              SchemeColor.onPrimaryContainer,
+          bottomNavigationBarSelectedIconSchemeColor:
+              SchemeColor.primaryContainer,
+          bottomNavigationBarUnselectedIconSchemeColor:
+              SchemeColor.onPrimaryContainer,
+          navigationBarHeight: 55.0,
+        ),
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        // To use the Playground font, add GoogleFonts package and uncomment
+        // fontFamily: GoogleFonts.notoSans().fontFamily,
+      ),
+// If you do not have a themeMode switch, uncomment this line
+// to let the device system mode control the theme mode:
+// themeMode: ThemeMode.system,
+
+      initialRoute: LOGIN_ROUTE,
+      getPages: [
+        GetPage(name: LOGIN_ROUTE, page: () => LoginScreen()),
+        GetPage(name: SIGNUP_ROUTE, page: () => SignUpPage()),
+        GetPage(name: DETAIL_ROUTE, page: () => DetailPage()),
+        GetPage(
+            name: HOME_ROUTE,
+            page: () => HomePage(),
+            transition: Transition.zoom),
+      ],
+    );
+  }
+}
---
+++ lib/lib/ui/pages/menuPage.dart
@@ -0,0 +1,122 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:greenplastic_app/constants.dart';
+import 'package:greenplastic_app/ui/pages/Cart_Catalogue/home.dart';
+import 'package:greenplastic_app/ui/pages/home.dart';
+import 'package:greenplastic_app/ui/controllers/login_controller.dart';
+
+class MenuPage extends StatefulWidget {
+  @override
+  _menuPageState createState() => _menuPageState();
+}
+
+class _menuPageState extends State<MenuPage> {
+  GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _globalKey,
+      drawer: Drawer(
+        backgroundColor: Color3,
+        elevation: 0,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+                width: 250,
+                height: 35,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Get.to(HomePageCart());
+                    },
+                    child: Text(
+                      'Catálogo',
+                      style: Theme.of(context).textTheme.labelLarge,
+                    ))),
+            SizedBox(
+              height: 40,
+            ),
+            SizedBox(
+                width: 250,
+                height: 35,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Cotización',
+                      style: Theme.of(context).textTheme.labelLarge,
+                    ))),
+            SizedBox(
+              height: 40,
+            ),
+            SizedBox(
+                width: 250,
+                height: 35,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Historial',
+                      style: Theme.of(context).textTheme.labelLarge,
+                    ))),
+            SizedBox(
+              height: 40,
+            ),
+            SizedBox(
+                width: 250,
+                height: 70,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Formulario \n contacto',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.labelLarge,
+                    ))),
+            Spacer(),
+            SizedBox(
+                /*width: 35,
+                height: 35,*/
+                child: ElevatedButton(
+              onPressed: () {
+                Get.to(HomePage());
+              },
+              child: Icon(Icons.logout),
+            )),
+            SizedBox(
+              height: 35,
+            ),
+          ],
+        ),
+      ),
+      body: Stack(
+        children: [
+          IconButton(
+            onPressed: () {
+              _globalKey.currentState?.openDrawer();
+            },
+            icon: Icon(Icons.menu),
+            color: Color3,
+          ),
+          Center(
+              child: Column(
+            children: [
+              Image.asset(
+                'assets/GreenPlastic.png',
+                width: 200,
+                height: 200,
+              ),
+              Text(
+                'Green Plastic es un agente de cambio ambiental, revalorizamos retales plásticos de un solo uso mediante la transformación a elementos de larga vida, bajo un modelo de economía circular.',
+                textScaleFactor: 1,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
